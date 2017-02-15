a887ae484ebe8a65a763c52ca6913f44a873c505
Shree Yalamanchili <Shree@Shrees-TSheets-Mac.local>
2017-02-14 18:16:35 -0700

    First commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Shree Yalamanchili on 1/21/17.
+//  Copyright © 2017 Shree Yalamanchili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    // MARK: Actions
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            
+            // If "." floating point already exists, do not add another "." floating point
+            if (digit == ".") && (display.text!.range(of: ".") != nil) {
+                return
+            }
+            
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathematicalSymbol)
+        }
+        displayValue = brain.result
+        
+    }
+    
+    
+    
+    // Clear the display
+    @IBAction func clear(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        display.text = "0"
+    }
+    
+    
+}
+
